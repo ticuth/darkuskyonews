0f4fb2f16cb7935873a38116839a846c0e54cfaf
TIC UTH <gflores_uth@outlook.com>
2015-02-05 10:39:48 -0700

    Small changes in manifest.webapp

---
+++ js/app.js
@@ -0,0 +1,32 @@
+var app = {
+	initialize: function() {
+		app.loadMessages();
+	},
+	loadMessages: function() {
+		$.ajax({
+			url:'http://darkuskyo.net/api/sitemessages/index/format/json',
+			data:{},
+			dataType:'JSON'
+		}).done(function(data){
+			$('#messages').empty();
+			$('#messages').append('<div><a href="#" onclick="app.loadMessages()">Reload</a> </div>');
+			$.each(data,function(key,item){
+
+				$('#messages').append('<div>'+item.content+'</div>');
+			});
+			//console.log(data.textos);
+		}).error(function( event, jqxhr, settings, thrownError){
+			$('#messages').empty();
+			$('#messages').append('<div>Unable to connect to server.</div>');
+			$('#messages').append('<div><a href="#" onclick="app.loadMessages()">Reload</a> </div>');
+
+		});
+	},
+	bindEvents: function() {
+		
+	}
+};
+
+window.onload=function(){
+	app.initialize();
+}
